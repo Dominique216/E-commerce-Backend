463aaac9d83da20a4bbcd15f36c26465cd46cf50
Dominique Nix <dominiquenix@Dominiques-MacBook-Air.local>
2023-02-08 20:30:57 -0600

    updated readme and tag routes

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 # UTA-bootcamp-challenge13
-This is challenge 13 for UT Austin's Coding Bootcamp
+## Description 
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+## Installation
+1. Copy the SSH key from the Repo into a folder on your own computer
+2. Once the files are installed, open the folder in your personal workspace
+3. For this application you will have to install express, sequelize, mysql2, and dotenv in the package.json file
+4. Add a .env file to the main folder and input your mySQL username, and password
+5. Open the integrated terminal on the schema.sql and run source schema.sql.
+6. Open the integrated terminal on the server.js file and run node seeds/index.js, then run node server.js
+7. Open Insomina and the get, post, update, and delete requests should work for localhost/3001/api/categories, localhost/3001/api/products, and localhost/3001/api/tags. You can also run get routes on an individual id numbers by adding /:id to the end of the url. 
+## Usage
+[Walkthrough Video]()
+## License
+This project uses an MIT license. For more information click the license badge at the top of the README.
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,7 +6,6 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', async (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
   try {
     const tagsData = await Tag.findAll({
       include: [{model: Product, through: ProductTag}], 
@@ -22,7 +21,6 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
   try {
     const oneTag = await Tag.findByPk(req.params.id, {
       include: [{model:Product, through: ProductTag}], 
@@ -35,30 +33,62 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', async (req, res) => {
+router.post('/', (req, res) => {
   // create a new tag
-  try {
-    const newTag = await Tag.create(req.body);
-    res.status(200).json(newTag)
-  } 
-  catch(err) {
-    res.status(500).json(err)
-  }
+  Tag.create(req.body)
+    .then((tag) => {
+      if(req.body.productIds.length) {
+        const tagArr = req.body.productIds.map((product_id) => {
+          return {
+            tag_id: tag.id,
+            product_id 
+          };
+        });
+        return ProductTag.bulkCreate(tagArr);
+      }
+    })
+    .then((alltagIds) => res.status(200).json(alltagIds)) 
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+    })
+    
 });
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
-  try {
-    const updateTag = await Tag.update(req.body, {
-      where: {
-        id:req.params.id,
-      }
-    });
-    res.status(200).json(updateTag); 
-  } 
-  catch(err) {
-    res.status(500).json(err);
-  }
+  Tag.update(req.body, {
+    where: {
+      id:req.params.id,
+    }
+  })
+    .then((product) => {
+      return ProductTag.findAll({where: {tag_id: req.params.id}})
+    })
+    .then((pT) => {
+      const currProducts = pT.map(({product_id}) => product_id);
+      
+      const newProducts = req.body.productIds
+      .filter((product_id) => !currProducts.includes(product_id))
+      .map((product_id) => {
+        return {
+          tag_id: req.params.id, 
+          product_id,
+        };
+      });
+      const toBeRemoved = pT
+      .filter(({product_id}) => !req.body.productIds.includes(product_id))
+      .map(({id})=> id);
+      return Promise.all([
+        ProductTag.destroy({where: {id: toBeRemoved}}), 
+        ProductTag.bulkCreate(newProducts)
+      ]);
+    })
+    .then((updatedTags => res.status(200).json(updatedTags)))
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+    })
 });
 
 router.delete('/:id', async (req, res) => {
